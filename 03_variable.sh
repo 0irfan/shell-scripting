ae0223613ecb66b4fb1e6db760d60cf6215fa801
root <root@vagrant.vm>
2024-06-21 19:03:57 +0000

    variable

---
+++ 03_variable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is constant variable
+
+ name="Abdul qadir"
+
+  echo "My name is: $name"
+
+  # this is dynamic variable
+
+   host=$(hostname)
+
+    echo "This is My hostname: $host"
+
+   
+
